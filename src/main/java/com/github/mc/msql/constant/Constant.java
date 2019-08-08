2bf2cd992d77c6eb1cd2fa3597381c58885ce046
279201486@qq.com <front5176829>
2019-08-08 19:50:22 +0800

    md..

--- src/main/java/com/github/mc/msql/constant/Constant.java
+++ src/main/java/com/github/mc/msql/constant/Constant.java
@@ -1,10 +1,10 @@
 package com.github.mc.msql.constant;
 
 public class Constant {
-    public static final String MINSERT = "com.mc.orange.msql.annotations.MInsert";
-    public static final String MUPDATE = "com.mc.orange.msql.annotations.MUpdate";
-    public static final String MSELECT = "com.mc.orange.msql.annotations.MSelect";
-    public static final String MDELETE = "com.mc.orange.msql.annotations.MDelete";
+    public static final String MINSERT = "com.github.mc.msql.annotations.MInsert";
+    public static final String MUPDATE = "com.github.mc.msql.annotations.MUpdate";
+    public static final String MSELECT = "com.github.mc.msql.annotations.MSelect";
+    public static final String MDELETE = "com.github.mc.msql.annotations.MDelete";
     public static final String INSERT = "org.apache.ibatis.annotations.Insert";
     public static final String UPDATE = "org.apache.ibatis.annotations.Update";
     public static final String SELECT = "org.apache.ibatis.annotations.Select";
